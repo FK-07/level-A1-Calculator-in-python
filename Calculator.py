08767fe83c4669d65449709a4dca998a16038d0c
FK-07 <kazimli.frid2007@gmail.com>
2025-08-28 15:15:57 +0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+b=[+,-,*,/]
+a=float(input()), k, c=float(input())
+while True :
+    if k==b[0]:
+        print(a+c)
+    
+    elif k==b[1]:
+        print(a-c)
+    elif k==b[2]:
+        print(a*c)
+    elif:
+        print(a/c)
+    elif c==0:
+    
